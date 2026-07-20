f75dec34843308f12b11591034a2d75c35ea807b
Deepakgg007 <deepakg081999@gmail.com>
2026-07-20 14:08:28 +0530

    feat: tune Lenis smooth kinetic scrolling with weighted luxury momentum

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -374,11 +374,11 @@ function Index() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 1.4,
+      easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      wheelMultiplier: 1.0,
-      touchMultiplier: 2,
+      wheelMultiplier: 0.9,
+      touchMultiplier: 1.5,
     });
 
     let rafId: number;
